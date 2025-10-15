1697c5cd5bd9c45100924f4600aefccb39d2239b
DESKTOP-7N1KM5T\User <bjh713@khu.ac.kr>
2025-10-15 21:12:48 +0900

    Update : create_chain

--- interface/create_chain.py
+++ interface/create_chain.py
@@ -1,21 +1,20 @@
-# 파일명: create_chain.py
 from langchain_core.runnables import RunnableLambda
 from langchain.schema.output_parser import StrOutputParser
 
 
-def create_db_chain(db_select_prompt, call_vllm):
+def create_db_chain(db_select_prompt, call_llm):
     db_selector_chain = (
             db_select_prompt
-            | RunnableLambda(lambda x: call_vllm(x.to_string()))
+            | RunnableLambda(lambda pv: pv.to_string())
+            | RunnableLambda(lambda s: call_llm(s))
             | StrOutputParser()
     )
     return db_selector_chain
 
-
-def create_image_chain(image_detect_prompt, call_vllm):
-    image_request_chain = (
-            image_detect_prompt
-            | RunnableLambda(lambda x: call_vllm(x.to_string()))
-            | StrOutputParser()
-    )
-    return image_request_chain
+# def create_image_chain(image_detect_prompt, call_llm):
+#     image_request_chain = (
+#             image_detect_prompt
+#             | RunnableLambda(lambda x: call_llm(x.to_string()))
+#             | StrOutputParser()
+#     )
+#     return image_request_chain
